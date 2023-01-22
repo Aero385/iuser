c37b6842f000b5cb0ccfb17b570cd52539647c85
Stanislav Prokofjev <Prokostas@gmail.com>
2023-01-22 16:00:34 +0200

    added router + open userID

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,28 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react'
-import Card, { CardVariant } from './components/Card';
-import List from './components/List';
-import UserItem from './components/UserItem';
-import TodoItem from './components/UserItem';
-import UserList from './components/UserList';
-import { ITodo, IUser } from './types/types';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import UserPage from './components/UserPage';
+import TodoPage from './components/TodoPage';
+import { Link } from 'react-router-dom';
+import UserItemPage from './components/UserItemPage';
+import TodoItemPage from './components/TodoItemPage';
 
 const App = () => {
   
-  const [users, setUsers] = useState<IUser[]>([])
-  const [todos, setTodos] = useState<ITodo[]>([])
-
-  useEffect(()=> {
-    fetchUsers()
-    fetchTodos()
-  },[])
-
-  async function fetchUsers() {
-    try {
-      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-      setUsers(response.data)
-    } catch (e) {
-      alert(e)
-    }
-  }
-
-  async function fetchTodos() {
-    try {
-      const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      setTodos(response.data)
-    } catch (e) {
-      alert(e)
-    }
-  }
-
   return (
-    <div>
-      <Card width='200px' height='200px' variant={CardVariant.primary}>
-        <button>Button</button>
-      </Card>
-      <List 
-        items={users} 
-        renderItem={(user: IUser) => <UserItem user={user} key={user.id}/>}
-      />
-      <List 
-        items={todos}
-        renderItem={(todo: ITodo) => <TodoItem user={user} key={todo.id}/>}
-      />
-    </div>
+    <BrowserRouter>
+      <div>
+          <div>
+            <Link to={'/users'}>Users</Link>
+            <Link to={'/todos'}>Todo list</Link>
+          </div>
+          <Routes>
+            <Route path="/users" element={<UserPage/>}/>
+            <Route path="/todos" element={<TodoPage/>}/>
+            <Route path="/users/:id" element={<UserItemPage/>}/>
+            <Route path="/todos/:id" element={<TodoItemPage/>}/>
+          </Routes>
+      </div>
+    </BrowserRouter>
+   
   );
 }
 
---
+++ src/components/TodoItemPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC, useState, useEffect } from 'react';
+
+const TodoItemPage: FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default TodoItemPage;
